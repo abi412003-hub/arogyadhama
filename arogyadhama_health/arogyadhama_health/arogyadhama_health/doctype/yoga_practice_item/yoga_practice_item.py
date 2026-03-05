f660a836caabcbda632eb2cabc0c354d7f27aeb4
abi412003-hub <abi412003@gmail.com>
2026-03-06 00:31:34 +0530

    Delete arogyadhama_health directory

--- arogyadhama_health/arogyadhama_health/arogyadhama_health/doctype/yoga_practice_item/yoga_practice_item.py
+++
@@ -1,5 +0,0 @@
-from frappe.model.document import Document
-
-
-class YogaPracticeItem(Document):
-    pass
--- arogyadhama_health/arogyadhama_health/arogyadhama_health/events/patient.py
+++
@@ -1,7 +0,0 @@
-import frappe
-
-
-def after_insert(doc, method):
-    """Called after a new patient is created."""
-    # Auto-notify reception about new patient
-    pass
